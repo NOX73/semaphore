29e9efaa248454943c39ed4a8eed622f0a2d618f
Rozhnov Alexandr <nox73@ya.ru>
2012-11-03 23:50:19 +0400

    first version

--- lib/semaphore/the_semaphore.rb
+++ lib/semaphore/the_semaphore.rb
@@ -3,9 +3,33 @@ class TheSemaphore
 
   def initialize(limit)
     @limit = limit
+    @in_critical = 0
+    @mutex = Mutex.new
   end
 
   def synchronize
-
+    until critical_in
+      sleep 0.1
+    end
+    yield
+    critical_out
   end
+
+  private
+
+    def critical_in
+      entered = false
+      @mutex.synchronize {
+        return if @in_critical == @limit
+        @in_critical += 1
+        entered = true
+      }
+      entered
+    end
+
+    def critical_out
+      @mutex.synchronize{
+        @in_critical -= 1
+      }
+    end
 end
--- test/lib/semaphore_test.rb
+++ test/lib/semaphore_test.rb
@@ -3,14 +3,48 @@ require 'test_helper'
 class SemaphoreTest < MiniTest::Unit::TestCase
 
   def test_should_create_semaphore
-    Semaphore.new 3
+    semaphore = Semaphore.new 3
+    assert semaphore
   end
 
   def test_should_synchronize_method
+    var = false
+
     semaphore = Semaphore.new 3
     semaphore.synchronize {
-
+      var = true
     }
+
+    assert var
+  end
+
+  def test_check_two_threads_write
+    var = []
+    semaphore = Semaphore.new 1
+
+    a = Thread.new{
+      semaphore.synchronize {
+        sleep 1
+        100.times{ var << 1 }
+        sleep 100
+      }
+    }
+
+    sleep 1
+
+    b = Thread.new{
+      semaphore.synchronize {
+        100.times{ var << 2 }
+      }
+    }
+
+    sleep 1
+
+    a.kill
+    b.kill
+
+    assert_equal 100, var.inject(:+)
+
   end
 
 end
